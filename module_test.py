c6d2c0e9529eaf0b43bc535ea61a44b5495cd89f
Janice-M <janiceink001@gmail.com>
2019-07-16 12:49:08 +0300

    check for nothing

--- module_test.py
+++ module_test.py
@@ -7,3 +7,13 @@ age = sys.argv[3]
 print(name)
 print(last)
 print(age)
+#how sys works
+
+#below is a check for nothing thing
+babe = ""
+list_a = []
+
+if list_a:
+    print("I will not run")
+else:
+        print("I am Empty")
