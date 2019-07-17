2bef0aad7455bf9cbd5df916af6826b2584895e9
Janice-M <janiceink001@gmail.com>
2019-07-17 12:48:32 +0300

    fuck string

--- module_test.py
+++ module_test.py
@@ -90,3 +90,19 @@ name = "Grey Worm"
 age = 9
 
 print(f"My name is {name} and I am {age} years old")
+#raw string 
+print('Beyonce\'s lemonade stand')
+    
+alpha = "I like old music"
+password = "K34jndnks"
+number_string = "12345"
+tabbs = "       "
+titles = "I Love Cups"
+false_titles = "I love Cups"
+
+print( alpha.isalpha() )
+print( password.isalnum() )
+print( number_string.isdecimal() )
+print( tabbs.isspace() )
+print( titles.istitle() )
+print( false_titles.istitle())
